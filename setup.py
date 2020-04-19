50d50afd18b05446756d920d2a8c9a2be8d922bf
KARTHEEK <kartheek2000mike@gmail.com>
2020-04-19 23:55:15 +0530

    Fixed package name

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
-setup(name='pymr',
+setup(name='pyMR',
       version='0.1',
       description='Testing installation of Package',
       url='#',
       author='k4rth33k',
       author_email='kartheek2000mike@gmail.com',
       license='MIT',
-      packages=['pyMR'],
+      packages=['pymr'],
       zip_safe=False)
