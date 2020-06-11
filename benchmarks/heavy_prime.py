69df84716c54a2958da8d65a3312c102a1920259
KARTHEEK <kartheek2000mike@gmail.com>
2020-06-11 15:15:35 +0530

    Added memory profiler to benchmark

--- benchmarks/heavy_prime.py
+++ benchmarks/heavy_prime.py
@@ -4,15 +4,19 @@ import tracemalloc
 import time
 import datetime
 import pandas as pd
+import tracemalloc
 
 
 def time_this(target):
-    def inner(x):
+    def inner(*args):
+        tracemalloc.start()
         start = datetime.datetime.now()
-        target(x)
+        target(*args)
         end = datetime.datetime.now()
+        current, peak = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
         delta = end - start
-        return delta.total_seconds()
+        return delta.total_seconds(), peak / 10**6
 
     return inner
 
@@ -37,8 +41,8 @@ def red_prime(primes1, primes2):
 
 
 @time_this
-def with_pymr(high_bound):
-    master = Master(10, 3, verbose=False)
+def with_pymr(high_bound, x):
+    master = Master(num_workers=9 + x, granularity=3 * x, verbose=False)
     master.create_job(data=range(high_bound),
                       map_fn=map_prime,
                       red_fn=red_prime)
@@ -51,16 +55,20 @@ def without_pymr(high_bound):
 
 
 def main():
-    df = pd.DataFrame(columns=['Range', 'Without pyMR', 'With pyMR'],
-                      index=False)
+    df = pd.DataFrame(columns=[
+        'Range', 'Raw (sec)', 'pyMR (sec)', 'Raw peek (MB)', 'pyMR peek (MB)'
+    ])
 
     for i in range(2, 10):
         print(f'Finding prime numbers in range - {10 ** i}')
-        with_time = with_pymr(10**i)
-        without_time = without_pymr(10**i)
-        df_ = pd.DataFrame([[10**i, without_time, with_time]],
-                           columns=['Range', 'Without pyMR', 'With pyMR'],
-                           index=False)
+        with_time, with_peek = with_pymr(10**i, i)
+        without_time, without_peek = without_pymr(10**i)
+        df_ = pd.DataFrame(
+            [[10**i, without_time, with_time, without_peek, with_peek]],
+            columns=[
+                'Range', 'Raw (sec)', 'pyMR (sec)', 'Raw peek (MB)',
+                'pyMR peek (MB)'
+            ])
         df = df.append(df_)
 
     print('-' * 50)
