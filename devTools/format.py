ee0552b587348db11fdba0bcbb9bdceeeb3c99ba
KARTHEEK <kartheek2000mike@gmail.com>
2020-07-18 22:55:03 +0530

    Added Badges

--- devTools/format.py
+++
@@ -1,9 +0,0 @@
-# root/scripts/format.py
-
-import os
-
-for root, subdirs, files in os.walk('.'):
-    for file in files:
-        if '.py' in file:
-            print(f'Formatting {root}/{file}')
-            os.system(f'yapf -i {root}/{file}')
